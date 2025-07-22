d64d17a6715d0835d5e0dc7bba190d207725104d
surya-3535 <bandojusuryaprakash3528@gmail.com>
2025-07-22 12:40:33 +0530

    Day 1 - Python Basics

---
+++ day-1.py
@@ -0,0 +1,32 @@
+print("hello, Surya welcome to python")
+name = "Surya" #str
+age = 21 #int
+height = 5.4 #float
+is_student = True #boolean
+print("Name:",name) #printing the name
+print("Age:",age) #printing the age
+print("Height:",height) #printing the height
+print("Student:",is_student) #printing the student or not
+
+# type checking
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#basic math operations
+a = 5
+b = 9
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Exponent:",a**b)
+
+#String operations
+greeting = "Hello"
+target = "World"
+message = greeting+" "+target+"!"
+print(message)
